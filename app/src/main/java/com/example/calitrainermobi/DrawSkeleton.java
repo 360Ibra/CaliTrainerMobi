b1aef09b348fa868f419e730666c39ab4729d307
Ibrahim <ibrahimaminu2k17@gmail.com>
2024-01-28 23:19:15 +0000

    DrawSkeleton - Keypoint linking begun

--- app/src/main/java/com/example/calitrainermobi/DrawSkeleton.java
+++ app/src/main/java/com/example/calitrainermobi/DrawSkeleton.java
@@ -12,36 +12,59 @@ public class DrawSkeleton {
 // Testing Git VCS between laptop and Desktop
     public static void drawPoseTrackerResult(org.opencv.core.Mat frame,mmdeploy.PoseTracker.Result[] results){
 
-        int skeleton[][] = {{15, 13}, {13, 11}, {16, 14}, {14, 12}, {11, 12}, {5, 11}, {6, 12},
-                {5, 6}, {5, 7}, {6, 8}, {7, 9}, {8, 10}, {1, 2}, {0, 1},
-                {0, 2}, {1, 3}, {2, 4}, {3, 5}, {4, 6}};
+        int skeleton[][] = {
+                {15, 13},
+                {13, 11},
+                {16, 14},
+                {14, 12},
+                {11, 12},
+                {5, 11},
+                {6, 12},
+                {5, 6},
+                {5, 7},
+                {6, 8},
+                {7, 9},
+                {8, 10},
+                {1, 2},
+                {0, 1},
+                {0, 2},
+                {1, 3},
+                {2, 4},
+                {3, 5},
+                {4, 6}};
+
+        int linkA = skeleton[0][0];
+        int linkB = skeleton[0][1];
+//     Iterate through the array without going out of bounds
+        System.out.println("Row Bounds" +skeleton.length);
+//      Printing out columns of the 2D array
+        System.out.println("Col Bounds" +skeleton[0].length);
 
         for(int i = 0; i < results.length; i++){
-            System.out.println("KEYPOINTS ARray" +results[i].keypoints);
-
+//          System.out.println("KEYPOINTS ARray" +results[i].keypoints);
             PointF[] keypointsArray = results[i].keypoints;
-
-            float[] scores = results[i].scores;
-
-            System.out.println(" Keypoint Prediction Scores"+ results[i].scores);
-
-
+//            float[] scores = results[i].scores;
 
             for (int j=0; j < keypointsArray.length;j++){
-//  Keypoint array length 17
+//          Keypoint array length 17
+            Point pt = new Point(keypointsArray[j].x,keypointsArray[j].y);
+//      Indexing specific keypoints
+            Imgproc.circle(frame,pt,3,new Scalar(255,0,0),4);
+            Imgproc.putText(frame,""+j,pt,Imgproc.FONT_HERSHEY_COMPLEX,0.5,new Scalar(0,0,255),1,Imgproc.LINE_AA);
 
-                Point pt = new Point(keypointsArray[j].x,keypointsArray[j].y);
 
-                Imgproc.circle(frame,pt,3,new Scalar(255,0,0),4);
-                System.out.println("Look at 9this point"+ pt);
+            Imgproc.line(frame, new Point(keypointsArray[linkA].x,keypointsArray[linkA].y),new Point(keypointsArray[linkB].x,keypointsArray[linkB].y),new Scalar(255,0,0),2,Imgproc.LINE_AA);
 
-//                TODO
-//                Add threshold for keypoints scores
-//                System.out.printf("Array Length[%d] Keypoint Coordinates: [%f] [%f]",keypointsArray.length,keypointsArray[j].x,keypointsArray[j].y);
+//          System.out.println("Look at this point"+ pt);
+//           TODO
+//            Add threshold for keypoints scores
+//           System.out.printf("Array Length[%d] Keypoint Coordinates: [%f] [%f]",keypointsArray.length,keypointsArray[j].x,keypointsArray[j].y);
             }
-
         }
-        System.out.println("RESULT LENGTH" + results.length);
+
+
+
+//        System.out.println("RESULT LENGTH" + results.length);
 //        PointF pt = new PointF(res);
 
 
---
+++ app/src/main/java/com/example/calitrainermobi/Trigonometry.java
@@ -0,0 +1,12 @@
+package com.example.calitrainermobi;
+
+import org.opencv.core.Point;
+
+public class Trigonometry {
+
+    public static void calcAngle(Point angle1, Point angle2, Point angle3){
+
+
+
+    }
+}
