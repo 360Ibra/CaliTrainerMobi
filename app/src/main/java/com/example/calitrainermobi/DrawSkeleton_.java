84718382d87d6f66f8c0488f6917ffc3d9bebe1a
Ibrahim Aminu <113380840+360Ibra@users.noreply.github.com>
2024-04-07 17:58:09 +0100

    Repetition Counter Implemented

--- app/src/main/java/com/example/calitrainermobi/DrawSkeleton_.java
+++ app/src/main/java/com/example/calitrainermobi/DrawSkeleton_.java
@@ -1,5 +1,6 @@
 package com.example.calitrainermobi;
 import android.icu.text.UFormat;
+import android.util.Log;
 
 import mmdeploy.PointF;
 import mmdeploy.PoseTracker;
@@ -12,9 +13,13 @@ import com.example.calitrainermobi.Trigonometry;
 // Using Utility static class to allow use for providing a functionality thats not tied to a specific instance
 
 public class DrawSkeleton_ {
+   public static int RealRepCount = 0;
+   private static boolean angleProcessed = false;
 
     // Testing Git VCS between laptop and Desktop
     public static void drawPoseTrackerResult(org.opencv.core.Mat frame,mmdeploy.PoseTracker.Result[] results) {
+        String stage = "down";
+        int repCount = 0;
 
         int skeleton[][] = {
                 {15, 13},
@@ -69,24 +74,105 @@ public class DrawSkeleton_ {
 
             for (int j =0; j < res.keypoints.length; j++){
 
-//             Getting Elbow join angle
-                Point A = new Point(res.keypoints[8].x, res.keypoints[8].y );
-                Point B = new Point(res.keypoints[6].x, res.keypoints[6].y );
-                Point C = new Point(res.keypoints[10].x, res.keypoints[10].y );
+////             Lsit KEYPOINTS & Front Lever
+//                Point A = new Point(res.keypoints[0].x, res.keypoints[0].y );
+//                Point B = new Point(res.keypoints[11].x, res.keypoints[11].y );
+//                Point C = new Point(res.keypoints[15].x, res.keypoints[15].y );
+//
+//                Point A1 = new Point(res.keypoints[0].x, res.keypoints[0].y );
+//                Point B2 = new Point(res.keypoints[12].x, res.keypoints[12].y );
+//                Point C3= new Point(res.keypoints[16].x, res.keypoints[16].y );
 
 
-                Point A1 = new Point(res.keypoints[7].x, res.keypoints[7].y );
-                Point B2 = new Point(res.keypoints[5].x, res.keypoints[5].y );
+                //  Handstand KEYPOINTS
+//                Point A = new Point(res.keypoints[10].x, res.keypoints[10].y );
+//                Point B = new Point(res.keypoints[12].x, res.keypoints[12].y );
+//                Point C = new Point(res.keypoints[16].x, res.keypoints[16].y );
+//
+//                Point A1 = new Point(res.keypoints[9].x, res.keypoints[9].y );
+//                Point B2 = new Point(res.keypoints[11].x, res.keypoints[11].y );
+//                Point C3= new Point(res.keypoints[15].x, res.keypoints[15].y );
+
+
+
+
+                //Pull Up Keypoints
+                Point A = new Point(res.keypoints[10].x, res.keypoints[10].y );
+                Point B = new Point(res.keypoints[8].x, res.keypoints[8].y );
+                Point C = new Point(res.keypoints[6].x, res.keypoints[6].y );
+
+                Point A1 = new Point(res.keypoints[5].x, res.keypoints[5].y );
+                Point B2 = new Point(res.keypoints[7].x, res.keypoints[7].y );
                 Point C3= new Point(res.keypoints[9].x, res.keypoints[9].y );
+
 
 //                Imgproc.putText(frame," " + Trigonometry.calcAngle(A1,B2,C3) + "            ",A1,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 0, 255), 1, Imgproc.LINE_AA);
 //                Imgproc.putText(frame,"" + Trigonometry.calcAngle(A,B,C),A,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 0, 255), 1, Imgproc.LINE_AA);
                 Point pos = new Point(100,100);
                 Point pos1 = new Point(100,50);
+                Point pos2= new Point(50,300);
+                Point pos3 = new Point(res.keypoints[8].x, res.keypoints[8].y);
+                Point pos4 = new Point(res.keypoints[7].x, res.keypoints[7].y);
 
-                Imgproc.putText(frame,"Lshoulder + Lelbow + Lwrist angle =  " + Trigonometry.calcAngle(A1,B2,C3),pos,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
-                Imgproc.putText(frame,"Rshoulder + Relbow + Rwrist angle =  " + Trigonometry.calcAngle(A,B,C),pos1 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
 
+                double result1 = Trigonometry.calcAngle2(A1,B2,C3);
+                double result2 = Trigonometry.calcAngle2(A,B,C);
+
+
+//              Pull Ups Calculation
+                if (result1 > 150.0 && result2 > 150.0) {
+                    stage = "down";
+                    angleProcessed = false;
+                } else if (result1 < 60.0 && result2 < 60.0 && stage.equals("down") && !angleProcessed) {
+                    stage = "up";
+                    repCount++;
+                    RealRepCount += repCount;
+                    angleProcessed = true;
+                } else if (result1 > 60.0 && result2 > 60.0 && stage.equals("up")) {
+                    stage = "down";
+                }
+
+//              Push Up Calculation
+//                if (result1 > 150.0 && result2 > 150) {
+//                    stage = "down";
+//                    angleProcessed = false;
+//                } else if (result1 < 80 && result2 < 80 && stage.equals("down") && !angleProcessed) {
+//                    stage = "up";
+//                    repCount++;
+//                    RealRepCount += repCount;
+//                    angleProcessed = true;
+//                } else if (result1 > 80 && result2 > 80 && stage.equals("up")) {
+//                    stage = "down";
+//                }
+
+                Imgproc.putText(frame,""+result2 ,pos3,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 0, 255), 1, Imgproc.LINE_AA);
+                Imgproc.putText(frame,""+result1 ,pos4,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 0, 255), 1, Imgproc.LINE_AA);
+
+                System.out.println("RepCount: " + repCount + " Angle: "+ result1+ " Stage: "+ stage  );
+                Log.d("RepSet", "RepCount: " + RealRepCount + " Angle: "+ result1+ " Stage: "+ stage);
+
+                Imgproc.putText(frame,"Rep Count: " +RealRepCount ,pos2 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 0, 255), 1, Imgproc.LINE_AA);
+
+
+
+//                if ((result1 > 95) && (result2 > 95)){
+//                    Imgproc.putText(frame,"OUT OF RANGE" + Trigonometry.calcAngle2(A1,B2,C3) + " " +Trigonometry.calcAngle2(A,B,C),pos2 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(255,0 , 0), 1, Imgproc.LINE_AA);
+//                    System.out.print("OUT OF RANGE" + Trigonometry.calcAngle2(A1,B2,C3));
+//                }
+//
+//                else {
+//                    Imgproc.putText(frame,"GOOD" + Trigonometry.calcAngle2(A1,B2,C3) + " "+ Trigonometry.calcAngle2(A,B,C),pos2 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0,255 , 0), 1, Imgproc.LINE_AA);
+//                    System.out.print("IN RANGE" + Trigonometry.calcAngle2(A1,B2,C3));
+//                }
+
+//                Imgproc.putText(frame,"Lshoulder + Lelbow + Lwrist angle =  " + Trigonometry.calcAngle(A1,B2,C3),pos,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
+//                Imgproc.putText(frame,"Rshoulder + Relbow + Rwrist angle =  " + Trigonometry.calcAngle(A,B,C),pos1 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
+//
+//                Imgproc.putText(frame,"NEW_Angle  " + Trigonometry.calcAngle2(A1,B2,C3),pos,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
+//                Imgproc.putText(frame,"R" + Trigonometry.calcAngle2(A,B,C),pos1 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
+
+                    Imgproc.putText(frame," " +  result1,pos ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0,255 , 0), 1, Imgproc.LINE_AA);
+                    Imgproc.putText(frame," " + result2 ,pos1 ,Imgproc.FONT_HERSHEY_COMPLEX,1,new Scalar(0,255 , 0), 1, Imgproc.LINE_AA);
 
                 if (used[j] == Boolean.TRUE){
                     Point point = new Point(res.keypoints[j].x,res.keypoints[j].y);
--- app/src/main/java/com/example/calitrainermobi/Trigonometry.java
+++ app/src/main/java/com/example/calitrainermobi/Trigonometry.java
@@ -13,8 +13,19 @@ public class Trigonometry {
 
     double resultToDegree = result * (180/Math.PI);
     return resultToDegree;
+    }
 
 
+    public static double calcAngle2(Point A, Point B, Point C){
+
+        double result =  Math.atan2(C.y - B.y, C.x - B.x) - Math.atan2(A.y - B.y, A.x - B.x);
+
+        double resultToDegree = Math.abs(result * (180/Math.PI));
+
+        if (resultToDegree > 180.0 ){
+            resultToDegree = 360 - resultToDegree;
+        }
+        return resultToDegree;
 
     }
 }
